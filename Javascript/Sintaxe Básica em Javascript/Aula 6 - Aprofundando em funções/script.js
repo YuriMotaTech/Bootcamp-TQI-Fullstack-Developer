e2fad85d973cc7d56af07e024a681b816dc5bed0
YuriMotaTech <yuri_motamelo@hotmail.com>
2022-07-16 00:25:34 -0300

    Conclusão materiais Sintaxe Básica Javascript

---
+++ Javascript/Sintaxe Básica em Javascript/Aula 6 - Aprofundando em funções/script.js
@@ -0,0 +1,30 @@
+// TIPOS DE FUNÇÕES
+
+/* // Declarativas
+function f_declarativa() {
+    console.log('Sou uma função declarativa');
+}
+
+f_declarativa();
+
+// Expressão de função
+// com nomeação
+var f_nomeada = function f_nomeada(){
+    console.log('Sou uma função de expressão nomeada');
+}
+
+f_nomeada();
+
+// sem nomeação
+var f_nao_nomeada = function(){
+    console.log('Sou uma função de expressão não nomeada');
+}
+
+f_nao_nomeada();
+
+// Arrow function
+var f_arrow = () => {
+    console.log('Sou uma arrow function');
+}
+
+f_arrow(); */
